d77d3847de0e4ff720c50bae3f8d49860d7660c9
Rohit7718beep <rohitkumar44302@gmail.com>
2026-06-24 22:45:41 +0530

    updated DAY3 module

---
+++ DAY3/script.js
@@ -0,0 +1,74 @@
+console.log("This is a demo")
+
+// alert('welcome to my website')
+
+let username="rohit";
+console.log(username)
+username="kingbeast"
+console.log(username)
+
+const PI=3.14;
+
+var marks=25;
+
+
+// data types
+let city="ludhiana"
+let houseno=345
+console.log(typeof(city))
+console.log(typeof(houseno))
+
+let iseven=true;
+console.log(typeof(iseven))
+
+let decision=null;
+console.log(decision)
+
+let name;
+console.log(typeof(name))
+
+let college_name; //Snake case
+// let college-name; //kebab case
+let collegeName; //camelcase
+
+
+//operators
+//arithmetic,relational,logical
+
+let a=8;
+let b=4; //assignment operators also includes a+=4 etc.
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//logical operator
+
+age=30;
+console.log(age>20 && age<50);
+console.log(age>30 || age<12);
+
+let isodd=true;
+console.log(!isodd)
+
+//comparison operator
+let m=34;
+let n=3;
+console.log(m>n)
+console.log(m!=n)
+
+let q=5;
+let r="5";
+console.log(q==r);
+console.log(q===r)
+
+let j=34;
+let k=32;
+//increment and decrement
+j--;
+ console.log(j);
+ k--;
+ console.log(k);
+
+ 
